878a58e5d6d411ffed0c8c243a3caac2bcc91042
AnaMaria33 <amazonasrod@gmail.com>
2024-01-10 12:20:35 -0300

    Update miniblog

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -1,14 +1,12 @@
-import { db } from "../firebase/config";
-
 import {
     getAuth, 
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     updateProfile,
-    signOut
-} from 'firebase/auth';
+    signOut,
+} from "firebase/auth";
 
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 export const useAuthentication = () => {
     const [error, setError] = useState(null);
@@ -26,25 +24,30 @@ export const useAuthentication = () => {
        }
     }
 
+    // register
     const createUser = async (data) => {
         checkIfIsCancelled();
+
         setLoading(true);
+        
+        setCancelled(true);
         setError(null);
 
         try{
-            const {user} = createUserWithEmailAndPassword(
+            const {user} = await createUserWithEmailAndPassword(
                 auth, 
-                data.Email,
+                data.email,
                 data.password
-            )
+            );
 
             await updateProfile(user, {
                 displayName: data.displayName
-            })
-
+            });
+            
+            setLoading(false);
             return user;
-        }
-        catch(error)
+
+        } catch(error)
         {
             console.log(error.message);
             console.log(typeof error.message);
@@ -56,11 +59,49 @@ export const useAuthentication = () => {
             } else if(error.message.includes("email-already")){
                 systemErrorMessage = " Email já cadastrado.";
             } else{ 
-                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde."
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
             }
+            setLoading(false);
             setError(systemErrorMessage);
         }
-        setLoading(false)
+    };
+
+    // logout - sign out
+    const logout = () => {
+        checkIfIsCancelled();
+
+       signOut(auth);
+    }
+
+    // login - sign in
+
+    const login = async (data) => {
+        checkIfIsCancelled();
+
+        setLoading(true);
+        setError(false);
+
+       try {
+            await signInWithEmailAndPassword(auth, data.email, data.password);
+            setLoading(false);            
+       } catch(error) {          
+          console.log(error.message);
+          console.log(typeof error.message);
+        
+          let systemErrorMessage;  
+
+          if (error.message.includes("user-not-found")){
+            systemErrorMessage = "Usuário não encontrado."
+          } else if (error.message.includes("wrong-password")){
+            systemErrorMessage = "Senha incorreta."
+          } else {
+            systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde."
+          }
+
+          setError(systemErrorMessage);
+        }
+        
+        setLoading(false);
     };
 
     useEffect(() => {
@@ -72,5 +113,7 @@ export const useAuthentication = () => {
         createUser,
         error,
         loading,
-    }
+        logout,
+        login,
+    };
 };
